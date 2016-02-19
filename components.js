fb2abc7c3fa41b28197b4c4764261fb5eb225d67
Suman Barick <codotronix@gmail.com>
2016-02-19 23:56:58 +0530

    view change experiment successful
    TaskMan has a method named viewChanged. TaskMan passes this as a
    property to ToolsGroup. SelectView inside ToolsGroup uses this
    viewChanged function as a callback whenever its view is changed... Then
    TaskMan changes its own state with the changed View and delegates that
    view value to its child TimeHeader... And TimeHeader then does its
    task....

--- components.js
+++ components.js
@@ -5,9 +5,15 @@ var TimeHeader = React.createClass({
     render: function () {
         var months = [];
         var monthClass = '';
-        if (this.props.subheader == 'y') {
+        if (this.props.subHeader == 'y') {
             months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];      
             monthClass = "col-xs-30 monthCell";
+        } else if (this.props.subHeader == 'q') {
+            months = ['Jan', 'Feb', 'Mar'];      
+            monthClass = "col-xs-120 monthCell";
+        } else if (this.props.subHeader == 'm') {
+            months = ['Jan'];      
+            monthClass = "col-xs-360 monthCell";
         }
         
         return (
@@ -70,7 +76,7 @@ var TaskContainer = React.createClass({
         for(var i=0; i<5; i++) {        
             var newId = (new Date()).getTime() * Math.random();
             var taskName = "Task_" + Math.round(Math.random()*999999);
-            var taskColor = "rgb(" + Math.floor(Math.random()*255) + ', ' + Math.floor(Math.random()*255) + ', ' + Math.floor(Math.random()*255) + ')';
+            var taskColor = "rgb(" + Math.floor(Math.random()*155 + 100) + ', ' + Math.floor(Math.random()*155 + 100) + ', ' + Math.floor(Math.random()*155 + 100) + ')';
             var startDate = Math.floor(Math.random()*180);
             var endDate = Math.floor(Math.random()*180) + 180;
 
@@ -108,12 +114,61 @@ var TaskContainer = React.createClass({
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
+/****************************************************************************************
+*********************** Component:  ToolsGroup *********************************************
+****************************************************************************************/
+var ToolsGroup = React.createClass({
+    viewStateChanged: function (e) {
+        //console.log(e.target.value);
+        this.props.viewChanged(e.target.value);
+    },
+    render: function () {        
+        return (
+            <div className="col-xs-360 marginTop15">
+                <div className="col-xs-30">
+                    <select className="form-control" onChange={this.viewStateChanged}>
+                        <option value="y">Year</option>
+                        <option value="q">Quarter</option>
+                        <option value="m">Month</option>
+                    </select>
+                </div>
+            </div>
+        );
+    }
+});
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/****************************************************************************************
+*********************** Component:  TaskMan *********************************************
+****************************************************************************************/
+var TaskMan = React.createClass({
+    getInitialState: function () {
+        var year = this.props.year;
+        var subHeader = this.props.subHeader;
+        return ({year: year, subHeader:subHeader});
+    },
+    viewChanged: function (subHeader) {
+        console.log('setting the TaskMan state subHeader=' + subHeader);
+        this.setState({subHeader: subHeader});
+    },
+    render: function () {
+        return (
+            <div>
+                <ToolsGroup viewChanged={this.viewChanged}/>
+                <TimeHeader  year={this.state.year} subHeader={this.state.subHeader}/>
+                <TaskContainer />
+            </div>
+        );
+    }
+});
+/////////////////////////////////////////////////////////////////////////////////////////
+
  
 //ReactDOM.render(<TaskInnerContainer taskData={taskData}/>, document.getElementById('taskContainer'));
 ReactDOM.render(
-    <div>
-        <TimeHeader year="2016" subheader="y"/>
-        <TaskContainer />
-    </div>, 
-    document.getElementById('reactContainer'));
+    <TaskMan year="2016" subHeader="y"/>, 
+    document.getElementById('reactContainer')
+);
 
